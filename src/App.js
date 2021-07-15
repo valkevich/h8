ce3889e3498534d82a1da55e086c9258b15104a6
Nikita Valkevich <n.valkevich@icloud.com>
2021-07-15 20:03:18 +0300

    init: ready hw8

--- src/App.js
+++ src/App.js
@@ -3,7 +3,37 @@ import axios from "../../les/node_modules/axios";
 
 
 const App = () => {
-  
+  const  [login, setLogin] = useState('')
+  const  [password, setPassword] = useState('')
+  const  [user, setUser] = useState('')
+
+  const onSignIn = () => {
+    axios.post("http://localhost:3001/auth/sign-in", {
+      phone: login,
+      password: password,
+    })
+    .then((response) => {
+      setUser(response.data.email)
+    })
+    
+  }
+
+  const onLoginChange = (event) => {
+    setLogin(event.target.value)
+  }
+
+  const onPasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  return (
+    <>
+      <input type='text' placeholder='Login' onChange={onLoginChange}/> 
+      <input type='text' placeholder='Password' onChange={onPasswordChange}/> 
+      <button onClick={onSignIn}>Sign In</button>
+      <div>{user}</div>
+    </>
+  )
 }
 
 
